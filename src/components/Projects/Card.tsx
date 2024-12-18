33484e709ac13c2a0919412178d4410c2911943e
DaviMF29 <migueldavi313@gmail.com>
2024-12-18 13:41:42 -0300

    Add project logos to Cards and update layout for better presentation

--- src/components/Projects/Card.tsx
+++ src/components/Projects/Card.tsx
@@ -3,18 +3,26 @@ import './Card.css';
 import academia from '../../images/academia.png';
 import moviemetricks from '../../images/moviemetricks.png';
 import posterfy from '../../images/posterfy.png';
+import academia_logo from '../../images/academia_logo.gif';
+import moviemetricks_logo from '../../images/movie_logo.png';
+import posterfy_logo from '../../images/posterfy_logo.png';
 import { FaGithub, FaPlay } from 'react-icons/fa';
 
 const Card = () => {
   return (
     <div className="projects-container">
       <h1 className="projects-title">{t("projects")}</h1>
-      <p className='projects-description'>{t("projects_description")}</p>
+      <p className="projects-description">{t("projects_description")}</p>
       <div className="projects-cards">
+        
+        {/* Ultra Academy */}
         <div className="project-card">
           <img src={academia} alt="Academia" className="project-card-img" />
           <div className="project-card-content">
-            <h1>Ultra Academy</h1>
+            <div className="project-title-with-logo">
+              <img src={academia_logo} alt="Academia Logo" className="project-logo" />
+              <h1>Ultra Academy</h1>
+            </div>
             <h2>{t("info_site_academia")}</h2>
             <div className="action-button">
               <a href="https://davimf29.github.io/Academia/" target="_blank" rel="noopener noreferrer">
@@ -26,10 +34,15 @@ const Card = () => {
             </div>
           </div>
         </div>
+
+        {/* Movie Metricks */}
         <div className="project-card">
           <img src={moviemetricks} alt="Movie Metricks" className="project-card-img" />
           <div className="project-card-content">
-            <h1>Movie Metricks</h1>
+            <div className="project-title-with-logo">
+              <img src={moviemetricks_logo} alt="Movie Metricks Logo" className="project-logo" />
+              <h1>Movie Metricks</h1>
+            </div>
             <h2>{t("info_site_moviemetricks")}</h2>
             <div className="action-button">
               <a href="https://moviemetricks.vercel.app/" target="_blank" rel="noopener noreferrer">
@@ -41,10 +54,15 @@ const Card = () => {
             </div>
           </div>
         </div>
+
+        {/* Posterfy */}
         <div className="project-card">
           <img src={posterfy} alt="Posterfy" className="project-card-img" />
           <div className="project-card-content">
-            <h1>Posterfy</h1>
+            <div className="project-title-with-logo">
+              <img src={posterfy_logo} alt="Posterfy Logo" className="project-logo" />
+              <h1>Posterfy</h1>
+            </div>
             <h2>{t("info_site_posterfy")}</h2>
             <div className="action-button">
               <a href="https://avictormorais.github.io/posterfy/" target="_blank" rel="noopener noreferrer">
@@ -56,6 +74,7 @@ const Card = () => {
             </div>
           </div>
         </div>
+
       </div>
     </div>
   );
